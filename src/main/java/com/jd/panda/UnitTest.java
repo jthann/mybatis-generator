788d11c0e5a06cda6783b16a1944477e5e45570a
hanjuntao <cdhanjuntao@jd.com>
2016-04-19 10:44:13 +0800

    generator

--- src/main/java/com/jd/panda/UnitTest.java
+++ src/main/java/com/jd/panda/UnitTest.java
@@ -1,5 +1,9 @@
 package com.jd.panda;
 
+import com.jd.panda.dao.UserDao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by hanjuntao on 2016/4/19.
  */
@@ -7,8 +11,11 @@ public class UnitTest {
 
     public static void main(String[] args) {
 
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext(new String[]{"spring-config.xml"});
 
+        UserDao userDao = applicationContext.getBean(UserDao.class);
 
+        System.out.println(userDao);
 
     }
 }
